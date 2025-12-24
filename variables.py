5bbe72a988235abd5861e4ffa391384e32c56304
Lakshmeesh Patel K <131551508+lakshmeeshpatelk@users.noreply.github.com>
2025-12-24 23:22:23 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,74 @@
+print("Hello world!")
+print("I like burger")
+print("It's really good")
+
+#This is my first comment
+
+
+print("bada and sharan and good friends")
+
+
+print("lakshmeesh")
+
+myName = "Lakshmeesh"
+
+
+print(myName)
+
+
+
+#variable = a variable is a coantainer for a value, which acts as a value itself
+ 
+ # Strings
+
+MyName = "LAKSHMEESH"
+my_dogs_name = "july"
+email = "abcd@gmail.com"
+
+
+
+# Integers
+
+
+myAge = 21
+my_cgpa = 4
+
+
+
+# Float
+
+myHeight = 5.9
+
+# Boolean
+
+isTall = True
+isShort = True
+
+ #conditional statements
+
+if isTall:
+    print("You are tall")
+if isShort:
+    print("You are short")
+
+
+# f string(f string is used to combine a normal string with value of a variable)
+
+print(f"My height is {myHeight}")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
