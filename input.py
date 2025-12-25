96bd56538567bec1d8cf96692e8802cc116cfaf2
Lakshmeesh Patel K <131551508+lakshmeeshpatelk@users.noreply.github.com>
2025-12-25 16:28:43 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,34 @@
+#input() = A function that prompts the user to enter a data in console and takes that data as input and returns it as a string data type
+
+
+
+#name = input("Enter your name")
+#age = int(input("Enter your age"))
+
+#age = age + 1
+
+#print(f"your name is {name}")
+#print("Happy birthday to you")
+#print(f"your age is {age}")
+
+
+
+# calculating the area of rectangle
+
+
+#length = input("Enter the length of rectangle")
+#breadth = input("Enter the breadth of a rectangle")
+
+#area = int(length) * int(breadth)
+
+#print(f"The area of a reactangles is {area}cm")
+
+#shopping cart
+
+item = input("Enter the name of the item")
+quantity = int(input("enter the quantity of the items"))
+price = float(input("Enter the price of the item"))
+
+total_cost = quantity * price
+print(f"total cost of the {item} is {total_cost}")
+
